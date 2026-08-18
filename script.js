20dec85ba13fc57b0d192ab4d71eea59f943c4ba
Md. Azizul Hakim <aazizul924@gmail.com>
2026-08-18 22:32:33 +0600

    elementation

---
+++ script.js
@@ -0,0 +1,8 @@
+const input = document.getElementById("textInput");
+const generateBtn = document.getElementById("generateBtn");
+const downloadBtn = document.getElementById("downloadBtn");
+
+const canvas = document.getElementById("qrCanvas");
+const ctx = canvas.getContext("2d");
+
+const message = document.getElementById("message");
